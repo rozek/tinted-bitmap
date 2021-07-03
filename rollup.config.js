f1281909f4918dcdea081ca37e3845988956e9a3
Andreas Rozek <A.Rozek@gmx.de>
2021-07-03 17:31:32 +0200

    setup build environment

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input:    './src/tinted-bitmap.ts',
+  external: ['javascript-interface-library'],
+  output: [
+    { 
+      file:      './dist/tinted-bitmap.js',
+      format:    'umd', // builds for both Node.js and Browser
+      name:      'tintedBitmap', // required for UMD modules
+      globals:   { 'javascript-interface-library':'JIL' },
+      noConflict:true,
+      sourcemap: true,
+      exports:   'default',
+      plugins: [terser({ format:{ comments:false, safari10:true } })],
+    }
+  ],
+  plugins: [
+    resolve(), commonjs(), typescript(),
+  ],
+};
