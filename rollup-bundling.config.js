a7d48bd8846c96051399a18aa5c95d3b12a44260
Andreas Rozek <A.Rozek@gmx.de>
2021-07-05 13:18:38 +0200

    update build config

--- rollup-bundling.config.js
+++ rollup-bundling.config.js
@@ -3,22 +3,18 @@
 import commonjs   from '@rollup/plugin-commonjs'
 import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input:    './src/tinted-bitmap.ts',
-  external: ['javascript-interface-library'],
   output: [
     { 
-      file:      './dist/tinted-bitmap.js',
+      file:      './dist/tinted-bitmap.bundled.js',
       format:    'umd', // builds for both Node.js and Browser
       name:      'tintedBitmap', // required for UMD modules
-      globals:   { 'javascript-interface-library':'JIL' },
       noConflict:true,
       sourcemap: true,
-    },{
-      file:      './dist/tinted-bitmap.esm.js',
-      format:    'esm',
-      sourcemap: true,
+      plugins: [terser({ format:{ comments:false, safari10:true } })],
     }
   ],
   plugins: [
